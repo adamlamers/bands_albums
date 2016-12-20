3ed748c1fac379e58ea373962f2cc92f3c9fda94
Adam Lamers <adam@scanalyticsinc.com>
2016-12-20 16:40:39 -0600

    add migrations for bands, albums

---
+++ database/migrations/2016_12_20_223257_create_bands_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bands', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('name');
+            $table->date('start_date');
+            $table->string('website');
+            $table->date('end_date');
+            $table->boolean('still_active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bands');
+    }
+}
