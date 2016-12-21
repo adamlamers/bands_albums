d1a1f8b3c77891ef332c0012c5fcaf7c5f3d3047
Adam Lamers <adam@scanalyticsinc.com>
2016-12-20 23:46:41 -0600

    Make bands editable and creatable

--- app/Http/Controllers/BandController.php
+++ app/Http/Controllers/BandController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Band;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBand;
 
 use Illuminate\Http\Request;
 
@@ -15,7 +16,13 @@ class BandController extends Controller
      */
     public function create()
     {
-        return view('band.create');
+        return view('band.create', ['editing' => false]);
+    }
+
+    public function edit($id)
+    {
+        $band = Band::findOrFail($id);
+        return view('band.create', ['editing' => true, 'edit_id' => $band->id, 'band' => $band]);
     }
 
     /**
@@ -32,9 +39,21 @@ class BandController extends Controller
         return redirect()->action("BandController@doList");
     }
 
-    public function save()
+    public function save(StoreBand $request)
     {
-        //create or update band
+        if($request->has('edit_id'))
+            $band = Band::findOrFail($request->input('edit_id'));
+        else
+            $band = new Band();
+
+        $band->name = $request->input('name');
+        $band->start_date = $request->input('start_date');
+        $band->website = $request->input('website');
+        $band->still_active = $request->has('still_active');
+        $band->save();
+
+        $request->session()->flash('message', 'Saved band: ' . $band->name);
+        return redirect()->action("BandController@doList");
     }
 
     public function doList()
--- resources/views/band/create.blade.php
+++ resources/views/band/create.blade.php
@@ -1,9 +1,55 @@
-@extends('index')
+@extends('layouts.app')
 
 @section('title')
-Create a Band
+
+@if(!empty($editing) and $editing == true)
+    Edit Band
+@else
+    Create Band
+@endif
+
 @stop
 
 @section('content')
-test
+        <h1 class="title">
+            @if(!empty($editing) and $editing == true)
+                Edit Band
+            @else
+                Create Band
+            @endif
+        </h1>
+
+        <form action="/band/save" method="POST">
+            {{ csrf_field() }}
+
+            @if(!empty($editing) and $editing == true)
+                <input type="hidden" value="{{ $edit_id }}" name="edit_id">
+            @endif
+
+            <label class="label">Band Name</label>
+            <p class="control">
+            <input class="input" type="text" placeholder="Band Name" name="name" value="{{ $band->name or old('name') }}">
+            </p>
+
+            <label class="label">Start Date</label>
+            <p class="control">
+            <input class="input" type="text" placeholder="1994-12-12" name="start_date" value="{{ $band->start_date or old('start_date') }}">
+            </p>
+
+            <label class="label">Website</label>
+            <p class="control">
+            <input class="input" type="text" placeholder="http://band.website" name="website" value="{{ $band->website or old('website') }}">
+            </p>
+
+            <p class="control">
+                <label class="checkbox">
+                    <input type="checkbox" name="still_active" {{ ($band->still_active or (old('still_active') == 'on')) ? 'checked' : '' }}>
+                    Still Active
+                </label>
+            </p>
+
+            <p class="control">
+                <button class="button is-primary">Save</button>
+            </p>
+        </form>
 @stop
